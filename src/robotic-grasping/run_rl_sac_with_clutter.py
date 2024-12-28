41e5165f1a3f5dd70fbf667e7d4b811a7f40c7d7
Shaktiman <spacegamer1111@gmail.com>
2024-12-28 13:36:07 +0530

    Changes for final models

--- src/robotic-grasping/run_rl_sac_with_clutter.py
+++ src/robotic-grasping/run_rl_sac_with_clutter.py
@@ -513,7 +513,7 @@ class NiryoRobotEnv(gym.Env):
 
         # Check if the target object is graspable
         if self.graspable:
-            reward += 15.0
+            reward += 25.0
             self.done = True
             rospy.loginfo(f"Ending episode as target object is graspable after actions taken by the bot")
         # Reward for varying white pixel count or clutter density if timestep > 1
@@ -563,7 +563,7 @@ if __name__ == "__main__":
 
     logdir = "logs"
     # Set up SAC model with a specified buffer size
-    model = SAC("MultiInputPolicy", env, verbose=1, buffer_size=50000, tensorboard_log=logdir)  # Set buffer size here
+    model = SAC("MultiInputPolicy", env, verbose=1, buffer_size=50000, tensorboard_log=logdir, gamma=0.9)  # Set buffer size here
 
     # Set up a checkpoint callback to save the model periodically
     checkpoint_callback = CheckpointCallback(save_freq=1000, save_path='./logs/', name_prefix='niryo_sac_with_clutter')
